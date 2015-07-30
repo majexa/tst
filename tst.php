0021c2f37b63cc62d2dfffe2e8a5ce4a3e82c20a
masted <masted311@gmail.com>
2015-07-30 15:24:43 +0300

    Auto-commit on 30.07.2015 16:24:43

--- tst.php
+++ tst.php
@@ -5,7 +5,9 @@ $params = implode(' ', array_slice($_SERVER['argv'], 1));
 $run = dirname(__DIR__).'/run/run.php';
 $pm = dirname(__DIR__).'/pm/pm.php';
 if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['argv'][1] == 'plib')) {
-  if (empty($_SERVER['argv'][3])) throw new Exception('projectName param #3 not defined');
+  if (empty($_SERVER['argv'][3])) {
+    throw new Exception('projectName param #3 not defined. cmd: '.implode(' ',$_SERVER['argv']));
+  }
   if ($_SERVER['argv'][1] == 'proj' and $_SERVER['argv'][2] == 'g' and $_SERVER['argv'][3] == 'test') {
     print `$pm localServer createTestProject common`;
   }
