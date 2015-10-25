4d79be9e04beeb15b1937e9722502cec2ea7069a
masted <masted311@gmail.com>
2015-10-25 20:32:17 +0300

    Auto-commit on 25.10.2015 20:32:17

--- tst.php
+++ tst.php
@@ -2,8 +2,8 @@
 
 define('TESTING', true);
 $params = implode(' ', array_slice($_SERVER['argv'], 1));
-$run = dirname(__DIR__).'/run/run.php';
-$pm = dirname(__DIR__).'/pm/pm.php';
+$run = 'php '.dirname(__DIR__).'/run/run.php';
+$pm = 'php '.dirname(__DIR__).'/pm/pm.php';
 if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['argv'][1] == 'plib')) {
   if (empty($_SERVER['argv'][3])) {
     throw new Exception('projectName param #3 not defined. cmd: '.implode(' ',$_SERVER['argv']));
@@ -13,12 +13,13 @@ if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['a
   }
   $offset = $_SERVER['argv'][1] == 'plib' ? -1 : 0;
   if ($_SERVER['argv'][1] == 'plib') $includes = ' '.$_SERVER['argv'][4 + $offset];
-  else $includes = '';
-  $cmd = "php $run site {$_SERVER['argv'][3 + $offset]} \"new CliTestRunner('$params')\"$includes";
+  else $includes = ' tst';
+  $cmd = "$run site {$_SERVER['argv'][3 + $offset]} \"new CliTestRunner('$params')\"$includes";
+  print "$cmd\n";
   print `$cmd`;
 }
 else {
   $includes = 'tst';
   if (isset($_SERVER['argv'][1]) and $_SERVER['argv'][1] == 'lib') $includes .= ','.$_SERVER['argv'][2];
-  print `php $run "new CliTestRunner('$params')" $includes`;
+  print `$run "new CliTestRunner('$params')" $includes`;
 }
