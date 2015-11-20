6ee899568a857459fe095f837fee364514262f8d
masted <masted311@gmail.com>
2015-11-20 11:50:07 +0300

    Auto-commit on 20.11.2015 11:50:07

--- lib/tests/TestAllErrors.class.php
+++ lib/tests/TestAllErrors.class.php
@@ -5,7 +5,8 @@ class TestAllErrors extends NgnTestCase {
   function test() {
     $caption = '';
     foreach ((new AllErrors)->get() as $v) {
-      $caption .= $v['file']."\n".$v['body']."\n".$v['trace']."\n=======\n";
+      $caption .= $v['body']."\n".$v['trace']. //
+        (isset($v['params']['entryCmd']) ? "Entry cmd: ".$v['params']['entryCmd'] : '')."\n=======\n";
     }
     $this->assertTrue($caption === '', $caption);
   }
